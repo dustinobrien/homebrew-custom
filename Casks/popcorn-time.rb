5e06773de6da8babf881ab9ad3ccdf4c9b386378
Dustin O'Brien <duobrien@gmail.com>
2015-06-23 21:33:26 -0400

    fixed sha256 hash

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'popcorn-time' do
   version '0.3.7-2'
-  sha256 'a35b5402c84b69bf54c86c278c62d93d933506966c8c819e56f97877e4d2cfb7n'
+  sha256 'bcf82c8dba9864324fb75400cd5b7f8421196429dee7d9b4d61901e9947d2e7f'
 
   url "https://get.popcorntime.io/build/Popcorn-Time-#{version.gsub('-', '.')}-Mac.dmg", :user_agent => :fake
   appcast 'https://popcorntime.io/update.json',
